0e4fbaeca3360765946b550fd8fd6a3d7764250c
dagget20 <lopez@lopez.(none)>
2014-06-09 00:21:16 -0500

    uri corrected

--- oauth.rb
+++ oauth.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'json'
+require 'base64'
 
 
 
@@ -8,17 +9,21 @@ class Sabre
 	def self.get_token
 		
 		
-		client_id = ''
-		clien_secret = ''
-		credentials = Base64.encode64("#{client_id}:#{clien_secret}")
+		client_id = Base64.strict_encode64("")
+		client_secret = Base64.strict_encode64('')
+		credentials = Base64.strict_encode64("#{client_id}:#{client_secret}")
 
 	
-		post('https://api.sabre.com/v1/auth/token', :headers => {"User-Agent" => "tripea", "Authorization" => "Basic #{credentials}", "Content-type" => "application/x-www-form-urlencoded"}, :query => { grant_type: "client_credentials"})
+		req = post('https://api.test.sabre.com/v1/auth/token', 
+			:headers => {"User-Agent" => "tripea", "Authorization" => "Basic #{credentials}", "Content-type" => "application/x-www-form-urlencoded"},
+			:ssl_version => :TLSv1,
+			:verbose     => true,
+			:body => { :grant_type => 'client_credentials'})
 		
+		@@token = req['access_token']
 	end
 
 end
 
-client = Sabre.new
 response = Sabre.get_token
 puts response
