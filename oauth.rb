6688ba8692ea72a4fd201a2cbe91cbf086d9d656
dagget20 <juampalg85@gmail.com>
2014-06-16 18:48:25 -0500

    minimal changes

--- oauth.rb
+++ oauth.rb
@@ -1,26 +1,31 @@
 require 'httparty'
 require 'json'
 require 'base64'
+require 'certified'
 
 
 
 class Sabre
 	include HTTParty
 	def self.get_token
-		
-		
-		client_id = Base64.strict_encode64("")
-		client_secret = Base64.strict_encode64('')
+
+
+		client_id = Base64.strict_encode64("V1:a06u5y6gtrmlus8f:DEVCENTER:EXT")
+		client_secret = Base64.strict_encode64('Ax3C1Wwr')
 		credentials = Base64.strict_encode64("#{client_id}:#{client_secret}")
 
-	
-		req = post('https://api.test.sabre.com/v1/auth/token', 
+
+		req = post('https://api.test.sabre.com/v1/auth/token',
 			:headers => {"User-Agent" => "tripea", "Authorization" => "Basic #{credentials}", "Content-type" => "application/x-www-form-urlencoded"},
 			:ssl_version => :TLSv1,
 			:verbose     => true,
 			:body => { :grant_type => 'client_credentials'})
-		
+
 		@@token = req['access_token']
+ 	end
+
+	def get_destination_flight(options)
+		
 	end
 
 end
